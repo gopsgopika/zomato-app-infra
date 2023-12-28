29681817b54e5e74df1d2f29788e8552f202faa2
Gopika <gopika@gmail.com>
2023-12-28 16:59:14 +0000

    Monitoring port

--- main.tf
+++ main.tf
@@ -61,6 +61,16 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
 
   egress {
     from_port        = 0
